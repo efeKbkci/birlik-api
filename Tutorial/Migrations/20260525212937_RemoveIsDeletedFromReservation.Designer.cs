572674d678ab940bffa7618d61e319950c2ddf53
efkanefe <efkanefekabakcii@gmail.com>
2026-05-26 02:35:09 +0300

    database: update columns

--- Tutorial/Migrations/20260525212937_RemoveIsDeletedFromReservation.Designer.cs
+++ Tutorial/Migrations/20260525212937_RemoveIsDeletedFromReservation.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Tutorial.Context;
@@ -11,9 +12,11 @@ using Tutorial.Context;
 namespace Tutorial.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260525212937_RemoveIsDeletedFromReservation")]
+    partial class RemoveIsDeletedFromReservation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -196,6 +199,11 @@ namespace Tutorial.Migrations
                     b.Property<int>("PassengerId")
                         .HasColumnType("integer")
                         .HasColumnName("passenger_id");
+
+                    b.Property<string>("PassengerStatus")
+                        .IsRequired()
+                        .HasColumnType("text")
+                        .HasColumnName("passenger_status");
 
                     b.Property<int>("PickupStopId")
                         .HasColumnType("integer")
---
+++ Tutorial/Migrations/20260525230246_RemovePassengerStatusFromReservation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tutorial.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePassengerStatusFromReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "passenger_status",
+                table: "reservations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "passenger_status",
+                table: "reservations",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
